88c705e9f4fb942b737337854934c59bf8770aed
michey <michey_@mail.ru>
2016-03-08 14:15:21 +0000

    2016-03-08 LAST

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-	'qa.models',
+	'qa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,13 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Question(models.Model):
-	title = models.CharField()
+	title = models.CharField(max_length=255)
 	text = models.TextField()
 	added_at = models.DateField()
 	rating = models.IntegerField()
-	author = models.ForeignKey(User,
-		null=False, on_delete=models.SET_NULL)
-	likes = models.ManyToManyField(User)
+	author = models.ForeignKey(User, related_name="a",
+		null=True, on_delete=models.SET_NULL)
+	likes = models.ManyToManyField(User, related_name="l")
 
 class Answer(models.Model):
 	text = models.TextField()
@@ -16,4 +16,4 @@ class Answer(models.Model):
 	question = models.ForeignKey(Question,
 		null=False, on_delete=models.PROTECT)
 	author=models.ForeignKey(User,
-		null=False, on_delete=models.SET_NULL)
+		null=True, on_delete=models.SET_NULL)
