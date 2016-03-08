925d5e3c063184ade0815ca53dc0e5198cfcf383
michey <michey_@mail.ru>
2016-03-08 13:12:17 +0000

    2016-03-08

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+	'qa.models',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -57,8 +58,10 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+		'read_default_file' : '/home/box/web/etc/db.conf',
+	},
     }
 }
 
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+	title = models.CharField()
+	text = models.TextField()
+	added_at = models.DateField()
+	rating = models.IntegerField()
+	author = models.ForeignKey(User,
+		null=False, on_delete=models.SET_NULL)
+	likes = models.ManyToManyField(User)
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateField()
+	question = models.ForeignKey(Question,
+		null=False, on_delete=models.PROTECT)
+	author=models.ForeignKey(User,
+		null=False, on_delete=models.SET_NULL)
